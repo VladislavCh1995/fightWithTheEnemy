67a06fed5aa0d69013ad6a6cca814b243a6107ff
Vladislav Churilov <Nawaharjan@gmail.com>
2024-07-10 20:56:56 +1000

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fightWithTheEnemy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int heroHealth = 100;
+            int heroDamage = 5;
+            int enemyHealth = 50;
+            int enemyDamage = 10;
+
+            while (heroHealth > 0 && enemyHealth > 0)
+            {
+                heroHealth -= enemyDamage;
+                enemyHealth -= heroDamage;
+                Console.WriteLine($"У героя {heroHealth} единиц здоровья, а у противника {enemyHealth} единиц здоровья");
+                if (heroHealth <= 0 || enemyHealth <= 0)
+                {
+                    Console.WriteLine("Ничья");
+                }
+                else if (heroHealth <= 0)
+                {
+                    Console.WriteLine("Победил противник.");
+                }
+                else
+                {
+                    Console.WriteLine("Победил герой");
+                }
+            }
+        }
+    }
+}
